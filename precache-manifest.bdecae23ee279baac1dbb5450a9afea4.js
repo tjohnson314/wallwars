baf151d7b1ae31acd2386351ab7042a97156d84c
Nil <nil.mamano@gmail.com>
2020-07-24 01:50:19 -0500

    Updates

---
+++ precache-manifest.bdecae23ee279baac1dbb5450a9afea4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c68b4ef690f0615d86616e3e093441a7",
+    "url": "/WallWars/index.html"
+  },
+  {
+    "revision": "bf0f23d9ba715483acae",
+    "url": "/WallWars/static/css/main.d7a8cf8c.chunk.css"
+  },
+  {
+    "revision": "9659a4ae22a27b3d00ca",
+    "url": "/WallWars/static/js/2.7438ccd9.chunk.js"
+  },
+  {
+    "revision": "a805aa73c09f95714fc94698b80cd246",
+    "url": "/WallWars/static/js/2.7438ccd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf0f23d9ba715483acae",
+    "url": "/WallWars/static/js/main.a1554174.chunk.js"
+  },
+  {
+    "revision": "fa1b6493368ac7f72444",
+    "url": "/WallWars/static/js/runtime-main.ea104f14.js"
+  }
+]);
