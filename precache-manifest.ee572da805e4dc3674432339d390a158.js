063bdfb0a5369a4acf5c1ecb16f4f59b2e080063
Nil <nil.mamano@gmail.com>
2021-10-11 21:56:47 -0700

    Updates

---
+++ precache-manifest.ee572da805e4dc3674432339d390a158.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85257b327cd126b599a7fa4bdd92b8e4",
+    "url": "/wallwars/index.html"
+  },
+  {
+    "revision": "73e4508399ddecb379e6",
+    "url": "/wallwars/static/css/2.c1f53081.chunk.css"
+  },
+  {
+    "revision": "0a446d76729461ab1fd7",
+    "url": "/wallwars/static/css/main.76bea61c.chunk.css"
+  },
+  {
+    "revision": "73e4508399ddecb379e6",
+    "url": "/wallwars/static/js/2.bc7742c9.chunk.js"
+  },
+  {
+    "revision": "0b0de5d337f1f74b467622c1da473d0e",
+    "url": "/wallwars/static/js/2.bc7742c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a446d76729461ab1fd7",
+    "url": "/wallwars/static/js/main.7e5508f0.chunk.js"
+  },
+  {
+    "revision": "47f505962f733a5be564",
+    "url": "/wallwars/static/js/runtime-main.147b1d2a.js"
+  },
+  {
+    "revision": "b39b76f742697cf2d31f524a4de99b33",
+    "url": "/wallwars/static/media/blueBgDark.b39b76f7.jfif"
+  },
+  {
+    "revision": "6108cc32edb971a54d42dafcefc9dea8",
+    "url": "/wallwars/static/media/blueBgLight.6108cc32.jfif"
+  },
+  {
+    "revision": "6df49e4b4d380649bcf4f5fb313558bd",
+    "url": "/wallwars/static/media/moveSound.6df49e4b.mp3"
+  }
+]);
